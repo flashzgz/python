c0665892567b169d43dea93fc62131e30187df4a
flashzgz <atureganorodriguez@gmail.com>
2022-06-10 08:42:43 +0200

    actualizar

---
+++ edad.py
@@ -0,0 +1,7 @@
+edad=input("introduce tu edad:")
+nombre= input ("introduce tu nombre")
+edad= int(edad)
+if edad >= 18:
+    print("Hola", nombre,"eres mayor de edad")
+else:
+    print("Hola", nombre, "eres menor de edad")    
---
+++ saludador.py
@@ -0,0 +1,5 @@
+nombre= input ("Dime tu nombre:")
+if nombre=="Alberto": 
+    print ("Hola Alberto, Hola Hola")
+else:
+    print("adios")
